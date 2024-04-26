edbba0aa48ad301c1aeb4d802126f741f92b8c2c
Shashank <pshashank64@gmail.com>
2024-04-26 20:29:14 +0530

    frontend

---
+++ src/components/CRPC/crpc.js
@@ -0,0 +1,5 @@
+function CRPC() {
+    
+}
+
+export default CRPC;
--- src/services/crpcService.js
+++ src/services/crpcService.js
@@ -1,21 +1,19 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8000/api/student';
+const baseURL = 'http://localhost:8000/api/crpc';
 
-const studentService = {
+const CRPCService = {
 
   login: async (email, password, role) => {
     try {
       const response = await axios.post(`${baseURL}/sign-in`, { email, password, role }, {withCredentials: true});
 
       const session  = response.data;
-      console.log(session);
+      console.log(session.message);
 
       // Save session data in local storage
       localStorage.setItem('session', session.session.sessionID);
       localStorage.setItem('user', session.session.user.name);
-      localStorage.setItem('roll', session.session.user.roll);
-      localStorage.setItem('userId', session.session.user.id);
       localStorage.setItem('role', session.session.user.role);
 
       return response.data;
@@ -25,12 +23,9 @@ const studentService = {
   },
 
   logout: () => {
-    axios.post(`${baseURL}/logout`, {}, {withCredentials: true});
     // Remove session data from local storage on logout
     localStorage.removeItem('session');
     localStorage.removeItem('user');
-    localStorage.removeItem('roll');
-    localStorage.removeItem('userId');
     localStorage.removeItem('role');
     window.location.reload();
   },
@@ -51,16 +46,36 @@ const studentService = {
     return user;
   },
 
-  getUserId: () => {
-    const userId = localStorage.getItem('userId');
-    console.log(userId);
-    return userId;
+
+  viewAllNOCs: async () => {
+    try {
+      let data = [];
+      data = await axios.get(`${baseURL}/allNOCs`, {withCredentials: true});
+      // console.log(data.data);
+      return data;
+    } catch (error) {
+      throw error.response.data;
+    }
   },
 
-  getRoll: () => {
-    const roll = localStorage.getItem('roll');
-    return roll;
+  approveNoc: async (nocId) => {
+    try {
+      const data = await axios.post(`${baseURL}/noc/approve`, {nocId}, {withCredentials: true});
+      return data;
+    } catch (error) {
+      throw error.response.data;
+    }
+  },
+
+  rejectNoc: async (nocId, rejectedBy) => {
+    try {
+      const data = await axios.post(`${baseURL}/noc/reject`, {nocId, rejectedBy}, {withCredentials: true});
+      return data;
+    } catch (error) {
+      throw error.response.data;
+    }
   }
+  
 };
 
-export default studentService;
+export default CRPCService;
